5bdfb1d2c3efd48cdb6999d019919466bc1fc4eb
hfstarman <hfstarman@gmail.com>
2020-09-08 00:02:25 -0400

    initial snippet commit

---
+++ week4/script/bitcamp-snippet-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <a href="https://snippet-generator.app/">Snippet Generator</a>
+    </div>
+  )
+}
+
+export default App

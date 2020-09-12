c7468e380bdda3d6292257a4e98e147f7fba094b
hfstarman <hfstarman@gmail.com>
2020-09-11 21:12:55 -0400

    prepped code better for students

--- week4/script/bitcamp-snippet-app/src/components/SnippetOutput.js
+++ week4/script/bitcamp-snippet-app/src/components/SnippetOutput.js
@@ -1,51 +1,46 @@
 import React, { useContext, useEffect } from 'react'
-import VSCode from './VSCode'
-import Atom from './Atom'
 import Clipboard from 'clipboard'
 import { MainContext } from '../contexts/MainContext'
 
 function SnippetOutput() {
   //! Does not use Inputs
-  const { mode, updateMode, getModeProperty } = useContext(MainContext)
+  const { mode, updateMode, getModeProperty, getModeNames } = useContext(
+    MainContext
+  )
 
   useEffect(() => {
     new Clipboard('.app__btncopy')
   }, [])
 
-  const getTopButtonClass = (buttonName) => {
-    if (mode === buttonName)
-      return `app__button app__button--${buttonName} app__button--active`
-    return `app__button app__button--${buttonName}`
-  }
+  const ModeButtonsJSX = getModeNames().map((modeName) => {
+    const additionalStyle =
+      modeName === mode ? getModeProperty(mode, 'style') : {}
+    return (
+      <button
+        key={modeName}
+        className="app__topbtn"
+        onClick={() => updateMode(modeName)}
+        style={additionalStyle}
+      >
+        {modeName}
+      </button>
+    )
+  })
 
   return (
     <div className="app__half">
-      <div className="app__halftop">
-        <button
-          className={getTopButtonClass('vscode')}
-          onClick={() => updateMode('vscode')}
-        >
-          VSCode
-        </button>
-        <button
-          className={getTopButtonClass('atom')}
-          onClick={() => updateMode('atom')}
-        >
-          Atom
-        </button>
-      </div>
+      <div className="app__halftop">{ModeButtonsJSX}</div>
 
       <div className="app__halfbottom">
         {getModeProperty(mode, 'component')}
 
-        <div className="app__buttons">
-          <button
-            className="app__btn app__btncopy"
-            data-clipboard-target=".app__pre"
-          >
-            Copy snippet
-          </button>
-        </div>
+        <button
+          className="app__btncopy app__bottombtn"
+          data-clipboard-target=".app__pre"
+          style={getModeProperty(mode, 'style')}
+        >
+          Copy snippet
+        </button>
       </div>
     </div>
   )
